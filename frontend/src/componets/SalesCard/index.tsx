d7e24e7bac7a7e5721a7a1ca04fcc57165fecd5d
Richard Farias <richard.sfarias@outlook.com>
2022-07-17 13:37:23 -0300

    Lista de vendas

--- frontend/src/componets/SalesCard/index.tsx
+++ frontend/src/componets/SalesCard/index.tsx
@@ -5,6 +5,8 @@ import "react-datepicker/dist/react-datepicker.css";
 import './styles.css'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Sale } from '../../models/Sale';
+import { BASE_URL } from '../../utils/request';
 
 function SalesCard() {
 
@@ -12,15 +14,18 @@ function SalesCard() {
   // const min = new Date(new Date().setDate(new Date().getDate() - 365)); //para pegar data de 1 ano antes do ano vigente
   const [minDate, setMinDate] = useState(new Date());
   const [maxDate, setMaxDate] = useState(new Date());
+  // useState para armazenar a lista de vendas com nome do dado (sales) e  o nome da função que altera o dado(setSales)
+  // useState<Sale[]([])>  NOTA: Forma de fazer um useState ser do tipo lista com valor inicial uma lista vazia -> ([])
+  const [sales, setSales] = useState<Sale[]>([]);
+
 
   useEffect(() => {
-    axios.get("http://localhost:8080/Sales")// --> Faz uma requisição para o backEnd
+    axios.get(`${BASE_URL}/Sales`)// --> Faz uma requisição para o backEnd trazendo através da variável de ambiente  "http://localhost:8080"
       .then(response => {
-        console.log(response.data)
+        // console.log(response.data)
+        setSales(response.data.content);
       })
-  },[]);
-
-
+  }, []);
 
   return (
     <div className="dsmeta-card">
@@ -57,48 +62,33 @@ function SalesCard() {
               <th>Notificar</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
+          <tbody
+          /*
+          //Expressão React
+          // NOTA: .map serve para percorrer a lista e fazer operações para cada elemento da lista
+          // NOTA: Quando é feito uma redentização de conteúdo baseado em um lista cada elemento precisa ter um valor único de uma Key. (key = {})
+          // new Date(sale.date).toLocaleDateString() --> Data formatada
+          // {sale.amount.toFixed(2)} --> valor formatado
+          */
+          >
+            {sales.map(sale => {
+              return (
+                <tr key={sale.id}>
+                  <td className="show992">{sale.id}</td>
+                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td>{sale.sellerName}</td>
+                  <td className="show992">{sale.visited}</td>
+                  <td className="show992">{sale.deals}</td>
+                  <td>R$ {sale.amount.toFixed(2)}</td>
+                  <td>
+                    <div className="dsmeta-red-btn-container">
+                      <NotificationButton />
+                    </div>
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
-
         </table>
       </div>
 
